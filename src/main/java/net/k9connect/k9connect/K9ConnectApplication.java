2184688a85783eb84260006eb8bebb9cd17970ba
Aarion Ojeda <aarion.ojeda01@gmail.com>
2021-10-05 11:51:37 -0500

    initial commit

---
+++ src/main/java/net/k9connect/k9connect/K9ConnectApplication.java
@@ -0,0 +1,13 @@
+package net.k9connect.k9connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K9ConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K9ConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/k9connect/k9connect/K9ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.k9connect.k9connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K9ConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
