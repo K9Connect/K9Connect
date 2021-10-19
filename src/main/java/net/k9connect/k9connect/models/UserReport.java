c26cf2973663c789b4204258eec73154722175ec
Aarion Ojeda <aarion.ojeda01@gmail.com>
2021-10-19 01:21:30 -0500

    added report

--- src/main/java/net/k9connect/k9connect/models/UserReport.java
+++ src/main/java/net/k9connect/k9connect/models/UserReport.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Table(name = "user_reports")
@@ -27,5 +30,17 @@ public class UserReport {
 
     @Column(columnDefinition = "TEXT")
     @Getter @Setter private String reportText;
+
+    @Column(updatable = false)
+    @Getter @Setter
+    private LocalDateTime sent_date = LocalDateTime.now();
+
+    public String getReadableDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
+        String S = this.sent_date.format(formatter); // formats to 09/23/2009 13:53
+        return S;
+    }
 
 }
