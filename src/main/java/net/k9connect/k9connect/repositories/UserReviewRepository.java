d1e965cece65a6662218d2cddd9a5777e759331f
Michael Coyle <michael.david.coyle4@gmail.com>
2021-10-28 14:59:54 -0500

    Added method to get all reviews of single user.

--- src/main/java/net/k9connect/k9connect/repositories/UserReviewRepository.java
+++ src/main/java/net/k9connect/k9connect/repositories/UserReviewRepository.java
@@ -4,6 +4,9 @@ import net.k9connect.k9connect.models.User;
 import net.k9connect.k9connect.models.UserReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
     UserReview findByReviewerAndReviewed(User reviewer, User reviewed);
+    List<UserReview> findAllByReviewed(User reviewed);
 }
