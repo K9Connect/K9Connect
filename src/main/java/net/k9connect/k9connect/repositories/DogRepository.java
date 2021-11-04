10547d6277bf8cdc6c9e3ac9c63afbd7bec09d79
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-11-04 09:11:09 -0500

    updating my branch with the main branch

--- src/main/java/net/k9connect/k9connect/repositories/DogRepository.java
+++ src/main/java/net/k9connect/k9connect/repositories/DogRepository.java
@@ -20,6 +20,21 @@ public interface DogRepository extends JpaRepository<Dog, Long> {
 
     List<Dog> findDogsByBreedIsLike(String breed);
 
-    List<Dog> findDogsByBreedIsLikeAndGenderAndDetails_Has_certs(String breed, char gender, boolean hasCerts);
+//    List<Dog> findDogsByBreedIsLikeAndGenderAndDetails_Has_certs(String breed, char gender, boolean hasCerts);
+//    List<Dog> findDogsByDetails_Age (int age);
+//    List<Dog> findDogsByDetails_Has_certs (boolean certs);
+
+//
+//   @Query(value="select Dog.gender, Dog.breed, DogDetails .has_certs" +
+//           where Dog.id=DogDetails.id
+//    List<Dog> findByKeyword(@Param("keyword") String keyword);
+
+
+//    @Query(value="select breed, gender, name, age from Dog ,DogDetails Where Dog.id= DogDetails.id", nativeQuery=true)
+//    List<Dog> findByKeyword(@Param("keyword") String keyword);
+
+//    @Query(value = " select Dog d from dog",
+//            nativeQuery = true)
+//    List<Dog> findDogsByGenderAndBreed(@Param("keyword") String keyword);
 }
 
