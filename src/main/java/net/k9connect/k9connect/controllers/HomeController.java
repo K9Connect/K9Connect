23bd98b6ea992b98c479ef6e6d8be93239e4985e
Michael Coyle <michael.david.coyle4@gmail.com>
2021-11-05 20:42:37 -0500

    Updated userLocations method to filter dogs by breed request param.

--- src/main/java/net/k9connect/k9connect/controllers/HomeController.java
+++ src/main/java/net/k9connect/k9connect/controllers/HomeController.java
@@ -1,14 +1,15 @@
 package net.k9connect.k9connect.controllers;
 
+import antlr.StringUtils;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import net.k9connect.k9connect.models.Dog;
 import net.k9connect.k9connect.models.User;
 import net.k9connect.k9connect.repositories.UserRepository;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -37,7 +38,29 @@ public class HomeController {
     // Get user location info as JSON
     @RequestMapping(value = "/users.json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<User> userLocations() {
-        return userDao.findAll();
+    public List<User> userLocations(@RequestParam String breed) {
+        List<User> allUsers = userDao.findAll();
+
+        if (breed == null || breed.isEmpty()) {
+            return allUsers;
+        }
+
+        List<User> filteredUsers = new ArrayList<>();
+
+        for (User user : allUsers) {
+            List<Dog> dogs = user.getDogs();
+
+            for (Dog dog : dogs) {
+                String dogBreed = dog.getBreed().toLowerCase();
+                String searchBreed = breed.toLowerCase();
+
+                if (dogBreed.contains(searchBreed)) {
+                    filteredUsers.add(user);
+                    break;
+                }
+            }
+        }
+
+        return filteredUsers;
     }
 }
