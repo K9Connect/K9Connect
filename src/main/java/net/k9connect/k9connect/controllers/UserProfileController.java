0a4fbbae8cd87624d39315c83c11755e15bb4445
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2021-11-09 09:31:59 -0600

    handled merge conflict with main

--- src/main/java/net/k9connect/k9connect/controllers/UserProfileController.java
+++ src/main/java/net/k9connect/k9connect/controllers/UserProfileController.java
@@ -7,10 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -141,7 +138,7 @@ public class UserProfileController {
         User user = userDao.findByUsername(loggedInUser.getUsername());
 
 //        System.out.println(user);
-
+        user.setPassword("");
         model.addAttribute("user",user);
 
         return "users/manage";
